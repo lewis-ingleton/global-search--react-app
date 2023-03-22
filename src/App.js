523278c2624c117285da5b62a598271ad7ab057b
Lewis Ingleton <lewis.ingleton1@.com>
2023-03-22 20:54:23 +0000

    headers

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Footer from "./components/footer/Footer";
 function App() {
   return (
     <>
-      <h1>Global Search !!</h1>
-      <h2>Global Search !</h2>
-      <h3>Global Search !</h3>
-      <h4>Global Search !</h4>
+      <h1>H1: Global Search !!</h1>
+      <h2>H2: Global Search !</h2>
+      <h3>H3: Global Search !</h3>
+      <h4>H4: Global Search !</h4>
       <Footer />
     </>
   );
