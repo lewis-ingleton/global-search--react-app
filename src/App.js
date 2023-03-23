e37584132095acd25be6872d9280a05485b1a8eb
Lewis Ingleton <lewis.ingleton1@.com>
2023-03-23 18:25:04 +0000

    folder move

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './index.css'
 
 // COMPONENTS
 import Footer from "./components/footer/Footer";
-import ResponsiveLayout from './components/footer/responsive-layout/Responsive';
+import ResponsiveLayout from './components/responsive-layout/Responsive';
 
 function App() {
   return (
     <>
-      <h1>H1: Global Search !!!</h1>
+      <h1>H1: Global Search !</h1>
       <h2>H2: Global Search !</h2>
       <h3>H3: Global Search !</h3>
       <h4>H4: Global Search !</h4>
