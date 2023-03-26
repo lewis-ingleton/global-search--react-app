2df9c0d9e01c1e5c4a3be349cc1b8e3aa4095ab4
Lewis Ingleton <lewis.ingleton1@.com>
2023-03-26 21:04:18 +0100

    header.js tweaks

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -22,7 +22,8 @@ function Header({
             <div className="header">
                 <div className="headerWrapper">
                     <h2>Search the Globe</h2>
-                    <p>**User story** Global Search is the educational tool that gives you breaking news from all countries across the globe, as well as latest key data for all countries.
+                    <p>**User story** Global Search is the educational tool that gives you breaking news from all countries across the globe, as well as latest key data for all countries.</p>
+                    <p>Use the search bar to search for a country.
                     </p>
                     <SearchBar
                         onSearch={onSearch}
