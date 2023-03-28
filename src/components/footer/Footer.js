372cafc46712c371f2a289dba0860f1e72221c70
hoduola5 <hoduola5@gmial.com>
2023-03-28 20:05:39 +0100

    Added repository URL to the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,30 +1,26 @@
 import React from "react";
 
-// CSS 
-import './Footer.css'
+// CSS
+import "./Footer.css";
 
-// icon 
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+// icon
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 
 function Footer() {
-
-    return (
-        <>
-            <footer>
-                <p>
-                    Team Glober Coders | Global Search | 2023
-                </p>
-                <a href="https://github.com/lewis-ingleton/global-search" target="_blank" rel="noreferrer">Checkout the GitHub repo <OpenInNewIcon fontSize="small" /></a>
-
-            </footer>
-
-        </>
-
-    )
-
-
-
+  return (
+    <>
+      <footer>
+        <p>Team Glober Coders | Global Search | 2023</p>
+        <a
+          href="https://github.com/lewis-ingleton/global-search--react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Checkout the GitHub repo <OpenInNewIcon fontSize="small" />
+        </a>
+      </footer>
+    </>
+  );
 }
-
 
 export default Footer;
